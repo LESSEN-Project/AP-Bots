52a681f904b8fa399d6bd4c6fea82e0f10a260e1
myazann <mertyazan2867@gmail.com>
2024-11-28 13:49:35 +0100

    renamed files

---
+++ evaluation/indv_eval.py
@@ -0,0 +1,64 @@
+import json
+import os
+
+from evaluate import load
+from openai import OpenAI
+
+from utils.file_utils import oai_get_batch_res, parse_filename
+from utils.argument_parser import parse_args
+
+_, dataset, _, _ = parse_args()
+
+preds_dir = os.path.join("files", "preds")
+out_dir = os.path.join("evaluation", "files", "indv")
+os.makedirs(out_dir, exist_ok=True)
+file_out_name = os.path.join(out_dir, f"eval_{dataset.tag}.json")
+
+client = OpenAI()
+oai_get_batch_res(client)
+
+out_gts = dataset.get_gts()
+all_rouge_scores = {}
+
+rouge = load("rouge")
+
+if os.path.exists(file_out_name):
+    with open(file_out_name, "r") as f:
+        all_rouge_scores = json.load(f)
+else:
+    all_rouge_scores = dict()
+
+for file in os.listdir(preds_dir):
+
+    if file.startswith(dataset.tag) and file.endswith(".json"):
+
+        params = parse_filename(file, dataset.tag)
+        print(f"Model: {params['model']}, Retriever: {params['retriever']}, Features: {params['features']}, RS: {params['RS']}, K: {params['k']}")
+
+        if file in all_rouge_scores.keys():
+            print("Individual eval for this already concluded!")
+            continue
+
+        with open(os.path.join(preds_dir, file), "r") as f:
+            preds = json.load(f)["golds"]
+            preds = [p["output"] for p in preds]
+
+        if len(preds) != len(out_gts):
+            continue
+
+        rouge_res = []
+        for pred, gt in zip(preds, out_gts):
+            
+            score = rouge.compute(predictions=[pred], references=[gt])
+            rouge_res.append(score)
+
+        all_rouge_scores[file] = {
+            "params": params,
+            "rouge1": [r["rouge1"] for r in rouge_res],
+            "rouge2": [r["rouge2"] for r in rouge_res],
+            "rougeL": [r["rougeL"] for r in rouge_res],
+            "rougeLsum": [r["rougeLsum"] for r in rouge_res],
+        }
+        
+        with open(file_out_name, "w") as f:
+            json.dump(all_rouge_scores, f)
---
+++ evaluation/total_eval.py
@@ -0,0 +1,53 @@
+import json
+import os
+
+from evaluate import load
+from openai import OpenAI
+import pandas as pd
+
+from utils.file_utils import oai_get_batch_res, parse_filename
+from utils.argument_parser import parse_args
+
+_, dataset, _, _ = parse_args()
+
+preds_dir = os.path.join("files", "preds")
+out_dir = os.path.join("evaluation", "files", "total")
+os.makedirs(out_dir, exist_ok=True)
+
+client = OpenAI()
+oai_get_batch_res(client)
+
+out_gts = dataset.get_gts()
+all_res = []
+models = []
+cols = ["model", "features", "retriever", "RS", "k"]
+
+rouge = load("rouge")
+bleu = load("bleu")
+cols.extend(["rouge1", "rouge2", "rougeL", "rougeLsum", "bleu"])
+
+for file in os.listdir(preds_dir):
+
+    if file.startswith(dataset.tag) and file.endswith(".json"):
+
+        with open(os.path.join(preds_dir, file), "r") as f:
+            preds = json.load(f)["golds"]
+            preds = [p["output"] for p in preds]
+
+        if len(preds) != len(out_gts):
+            continue
+
+        params = parse_filename(file, dataset.tag)
+        print(f"Model: {params['model']}, Retriever: {params['retriever']}, Features: {params['features']}, RS: {params['RS']}, K: {params['k']}")
+
+        rouge_results = rouge.compute(predictions=preds, references=out_gts)
+        bleu_results = bleu.compute(predictions=preds, references=[[gt] for gt in out_gts])
+        res_dict = params | rouge_results
+        res_dict["bleu"] = bleu_results["bleu"]
+        
+        all_res.append(res_dict)
+
+df = pd.DataFrame(all_res)
+df = df[cols]
+df = df.round(dict([(c, 4) for c in df.columns if df[c].dtype == "float64"]))
+df.to_csv(os.path.join(out_dir, f"eval_{dataset.tag}.csv"), index=False, columns=cols)
