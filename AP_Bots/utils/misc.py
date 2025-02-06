abff0ef0a78f49976a2d63dbe377c93f3bd74569
myazann <mertyazan2867@gmail.com>
2025-02-06 09:21:58 +0100

    new results

--- AP_Bots/utils/misc.py
+++ AP_Bots/utils/misc.py
@@ -4,10 +4,8 @@ def softmax(x):
 
     e_x = np.exp(x - np.max(x))
     return (e_x/e_x.sum()).tolist()
-    
 
 def get_model_list():
 
-    return ["DEEPSEEK-R1-GGUF", "DEEPSEEK-R1-DISTILL-QWEN-32B", "LLAMA-3.1-8B", 
-            "GEMMA-2-9B", "GEMMA-2-27B", "LLAMA-3.3-70B", "MINISTRAL-8B-INSTRUCT", 
-            "LLAMA-3.2-3B", "GEMMA-2-2B"]
+    return [ "LLAMA-3.1-8B", "GEMMA-2-9B", "GEMMA-2-27B", "LLAMA-3.3-70B", 
+            "MINISTRAL-8B", "MINISTRAL-8B-INSTRUCT", "LLAMA-3.2-3B", "GEMMA-2-2B"]
